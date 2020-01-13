328b61f5ad96b2c6ce9f2bbf75da422ee2bda520
Collin <cojo2657@colorado.edu>
2020-01-13 12:04:00 -0700

    broken

---
+++ views/https.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || 
+   $_SERVER['HTTPS'] == 1) ||  
+   isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&   
+   $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))
+{
+   //$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+   // echo $_SERVER['HTTP_HOST'];
+   // echo $_SERVER['REQUEST_URI'];
+   header('HTTP/1.1 301 Moved Permanently');
+   header('Location: about.html');
+   exit();
+}
+?>
